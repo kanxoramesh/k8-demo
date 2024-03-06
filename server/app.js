46347c0a7074e8ad7228c0231ca45335648507c5
ramesh <ramesh_pokhrel@lftechnology.com>
2024-03-06 17:29:48 +0545

    update version2 for web

--- server/app.js
+++ server/app.js
@@ -1,9 +1,10 @@
 const app = require('express')();
+import os from 'os'
 
 const appid= process.env.PORT
 
 app.get('/', function (req, res) {
-  res.send(`Rep ?? called ${appid}: listening on port ${appid} `);
+  res.send(`version 2.0.0: called from ${os.hostname}: listening on port ${appid} `);
 });
 app.listen(appid, function () {
   console.log(`Example app listening on port ${appid}!`);
